68febddca604f46943d8b094cf0035565b28ea54
AaronMcDougall <72240339+AaronMcDougall@users.noreply.github.com>
2021-09-23 17:52:08 +1000

    Little housekeeping, added clear function to 2DPerlin scene

---
+++ Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    public GameObject cube;
+
+    private List<GameObject> grid = new List<GameObject>();
+
+    public int gridSize;
+
+    public float cubeHeight;
+    public float perlinAdjust;
+    public float scale;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateGrid();
+        new List<Grid>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void GenerateGrid()
+    {
+        for (int i = 0; i < gridSize; i++)
+        {
+            for (int y = 0; y < gridSize; y++)
+            {
+                cubeHeight = Mathf.PerlinNoise(i * perlinAdjust, y * perlinAdjust) * scale;
+                GameObject copy = Instantiate(cube, new Vector3(i, cubeHeight, y), Quaternion.Euler(0, 0, 0));
+                grid.Add(copy);
+            }
+        }
+    }
+
+    public void ClearGrid()
+    {
+        foreach (GameObject cube in grid)
+        {
+            GameObject.DestroyImmediate(cube, true);
+        }
+        grid.Clear();
+    }
+
+}
